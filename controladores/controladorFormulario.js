038e2c3a1eef305d6c18adf9c7d5d3d4095a2f50
felipe gonzalez <fgonzalez@cesde.net>
2024-04-30 11:16:47 -0500

    Se hicieron cambios en el JS

--- controladores/controladorFormulario.js
+++ controladores/controladorFormulario.js
@@ -1,8 +1,19 @@
 //scope de las variables de JS
-let nombre= "Felipe Diaz";
+//almacenando una etiqueta html en una variable de js
+let productPrizeBox=document.getElementById('prize');
+let productDescriptionBox=document.getElementById('Description');
+let productPhotoBox=document.getElementById('Photo');
+let productReviewBox=document.getElementById('Review');
+let productBrandBox=document.getElementById('Brand');
+let productOpcionBox=document.getElementById('optionID');
+let productButtonBox= document.getElementById('button');
 
-if(5>2){
-    nombre="Pablo Diaz"
-}
+//Como detecto un evento en js
+//Una vez tenga una variable que almacena la etiqueta sobre la cual quiero escuchar el evento, entonces se utiliza
+//La funcion o metodo (perro guardian)
+productButtonBox.addEventListener("click",function(evento){
+    evento.preventDefault();
+    //Logica que deseo activar si el evento se detecto con exito 
+    console.log("Acaban de darle click al formulario");
+});
 
-console.log("hola "+nombre);
